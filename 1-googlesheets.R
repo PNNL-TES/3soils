92a5cae9bfee9cfb3c0f8da90754cba82ed5e6d3
Ben Bond-Lamberty <bondlamberty@pnnl.gov>
2018-11-27 17:25:42 -0500

    Update all local data

--- 1-googlesheets.R
+++ 1-googlesheets.R
@@ -7,6 +7,7 @@ SCRIPTNAME  	<- "1-googlesheets.R"
 PROBLEM       <- FALSE
 
 library(googlesheets) # 0.2.2
+library(googledrive)
 
 # ==============================================================================
 # Main 
@@ -14,6 +15,39 @@ library(googlesheets) # 0.2.2
 openlog(file.path(outputdir(), paste0(SCRIPTNAME, ".log.txt")), sink = TRUE)
 
 printlog("Welcome to", SCRIPTNAME)
+
+# Notes   November 27, 2018
+# The do_download below downloads all Picarro files but note it's starting in my GDrive folder
+# The code below that is broken--they shifted which files were using for key information
+# (per Peyton email 20 November 2018)
+# For now I've just downloaded the relevant file (3Soils_CPCRW_SR_DWP_PicarroLog) directly
+
+
+printlog("Getting list of Picarro files...")
+folder_id <- as_id()
+filelist <- drive_ls("~/3Soils_Picarro/")
+
+do_download <- function(filelist, depth = 1) {
+  for(f in seq_len(nrow(filelist))) {
+    cat(depth, filelist$name[f], "\t")
+    if(is_folder(filelist[f,])) {
+      cat("folder\n")
+      do_download(drive_ls(as_id(filelist$id[f])), depth + 1)
+    } else {
+      localfile <- file.path("data/picarro/", filelist$name[f])
+      if(file.exists(localfile)) {
+        cat("already exists\n")
+      } else {
+        cat("downloading", filelist$name[f], "\n")
+        drive_download(filelist[f,], path = localfile)
+      }
+    }
+  }
+}
+
+suppressWarnings(do_download(filelist))
+
+
 
 # Register file - need to be authenticated to Google
 # Not storing an OAuth token here
