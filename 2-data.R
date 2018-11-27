8943e96f4adefcc22fa415d5aa8a9290b0104c99
Ben Bond-Lamberty <bondlamberty@pnnl.gov>
2018-11-27 18:49:02 -0500

    No really this time--robust to read errors

--- 2-data.R
+++ 2-data.R
@@ -11,35 +11,6 @@ PROBLEM       <- FALSE
 PICARRO_DATA_DIR      <- file.path(DATA_DIR, "picarro/")
 
 # -----------------------------------------------------------------------------
-# read a single output file, returning data frame
-read_outputfile <- function(fqfn) {
-  printlog("Reading", fqfn)
-  stopifnot(file.exists(fqfn))
-  
-  f <- fqfn
-  if(grepl(".gz$", fqfn)) {
-    f <- gzfile(fqfn)
-  } else if(grepl(".zip$", fqfn)) {
-    f <- unz(fqfn)
-  }
-  
-  d <- NULL
-  try(d <- read.table(f, header = TRUE))
-  if(is.null(d)) {
-    warning("Could not read", f)
-    return(d)
-  }
-  
-  print_dims(d)
-  
-  # Add ancillary data
-  d$file <- basename(fqfn)
-  #  d$dir <- dirname(fqfn)
-  
-  return(d)
-} # read_outputfile
-
-# -----------------------------------------------------------------------------
 # scan a directory and process all files in it, returning tempfile names
 process_directory <- function(input_path) {
   printlog("process_directory", input_path)
@@ -50,7 +21,12 @@ process_directory <- function(input_path) {
   filedata <- list()
   for(f in filelist) {
     printlog("Reading", f)
-    tibble::as_tibble(read.table(f, header = TRUE, stringsAsFactors = FALSE)) %>%
+    d <- try(read.table(f, header = TRUE, stringsAsFactors = FALSE), silent = TRUE)
+    if(inherits(d, "try-error")) {
+      warning("Couldn't read", f)
+      next
+    }
+    tibble::as_tibble(d) %>%
       # select only the columns we need, and discard any fractional valve numbers
       select(DATE, TIME, ALARM_STATUS, MPVPosition, CH4_dry, CO2_dry, h2o_reported) %>%
       filter(MPVPosition == floor(MPVPosition)) ->
