4daa1ea6d00ff79457759e483ebe671ad22f46e9
Ben Bond-Lamberty <bondlamberty@pnnl.gov>
2019-03-30 16:17:02 -0400

    Clean up script for final summaries

--- 5-fluxes.R
+++ 5-fluxes.R
@@ -39,7 +39,10 @@ sdata %>%
   replace_na(list(Additional_Wt_toRemove = 0)) %>% 
   mutate(sm_gravimetric = (Total_core_mass_pot_pie_pans_g - DryMass_SoilOnly_g - DryMass_NONsoil_ALL_g - Additional_Wt_toRemove) / DryMass_SoilOnly_g,
          sm_volumetric = (Total_core_mass_pot_pie_pans_g - DryMass_SoilOnly_g - DryMass_NONsoil_ALL_g - Additional_Wt_toRemove) / VolumeSoil_cm3,
-         bd = DryMass_SoilOnly_g / VolumeSoil_cm3) -> 
+         bd = DryMass_SoilOnly_g / VolumeSoil_cm3,
+         sm_gravimetric = round(sm_gravimetric, 3),
+         sm_volumetric = round(sm_volumetric, 3),
+         bd = round(bd, 3)) -> 
   sdata
 
 # Summary plots and table
@@ -136,7 +139,7 @@ sdata %>%
   arrange(DATETIME) %>% 
   group_by(SampleID, TREATMENT_PHASE) %>% 
   # Compute incubation time
-  mutate(inctime_hours = as.numeric(difftime(DATETIME, min(DATETIME), units = "hours")),
+  mutate(inctime_hours = as.numeric(difftime(DATETIME, min(DATETIME), units = "hours")) %>% round(2),
          # interpolate missing fluxes
          CO2_flux_mgC_hr_interp = approx(inctime_hours, CO2_flux_mgC_hr, xout = inctime_hours, rule = 2)[['y']],
          CH4_flux_mgC_hr_interp = approx(inctime_hours, CH4_flux_mgC_hr, xout = inctime_hours, rule = 2)[['y']],
@@ -154,107 +157,68 @@ save_data(fluxdata, fn = FLUXDATA_FILE, scriptfolder = FALSE)
 
 # Peyton's cutoffs from 2018-12-04 email
 fluxdata %>% 
+  select(Site, TREATMENT_PHASE, SampleID, DATETIME, inctime_hours,
+         sm_gravimetric, sm_volumetric, 
+         `CO2 flux (mgC/hr)` = CO2_flux_mgC_hr, 
+         `CH4 flux (mgC/hr)` = CH4_flux_mgC_hr, 
+         `cumCO2 flux (mgC/gSoil)` = cumCO2_flux_mgC_gSoil, 
+         `cumCH4 flux (mgC/gSoil)` = cumCH4_flux_mgC_gSoil, label) %>% 
+  arrange(Site, TREATMENT_PHASE, SampleID, inctime_hours) %>% 
   mutate(cutoff = if_else(TREATMENT_PHASE %in% c("SATURATION_IMMEDIATE", 
-                                       "SATURATION_SATURATION", 
-                                       "FIELD_MOIST_SATURATION", 
-                                       "DROUGHT_SATURATION"), 7.5, 999),
+                                                 "SATURATION_SATURATION", 
+                                                 "FIELD_MOIST_SATURATION", 
+                                                 "DROUGHT_SATURATION"), 7.5, 999),
          cutoff = if_else(TREATMENT_PHASE %in% c("DROUGHT_INCUBATION", 
-                                       "FIELD_MOIST_INCUBATION", 
-                                       "SATURATION_INCUBATION"), 600, cutoff)) %>% 
+                                                 "FIELD_MOIST_INCUBATION", 
+                                                 "SATURATION_INCUBATION"), 600, cutoff)) %>% 
   filter(inctime_hours <= cutoff) ->
   fluxdata_cutoff
+save_data(fluxdata_cutoff)
 
-
-p_collar_co2 <- ggplot(fluxdata_cutoff, aes(inctime_hours, cumCO2_flux_mgC_gSoil, group = SampleID, color = Site)) + 
+p_collar_co2 <- ggplot(fluxdata_cutoff, aes(inctime_hours, `cumCO2 flux (mgC/gSoil)`, group = SampleID, color = Site)) + 
   geom_point() + geom_line() + geom_text(aes(x = inctime_hours * 1.1, label = label), size = 3) +
   facet_wrap(~TREATMENT_PHASE, scales = "free") + theme(strip.text = element_text(size = 7)) +
   ggtitle("Cumulative CO2 emissions by core")
 print(p_collar_co2)
-save_plot("cumulative_co2_inc", width = 8, height = 6)
+save_plot("cumulative_co2_by_core", width = 8, height = 6)
 
-p_collar_ch4 <- ggplot(fluxdata_cutoff, aes(inctime_hours, cumCH4_flux_mgC_gSoil, group = SampleID, color = Site)) + 
+p_collar_ch4 <- ggplot(fluxdata_cutoff, aes(inctime_hours, `cumCH4 flux (mgC/gSoil)`, group = SampleID, color = Site)) + 
   geom_point() + geom_line() + geom_text(aes(x = inctime_hours * 1.1, label = label), size = 3) +
   facet_wrap(~TREATMENT_PHASE, scales = "free") + theme(strip.text = element_text(size = 7)) +
   ggtitle("Cumulative CH4 emissions by core")
 print(p_collar_ch4)
-save_plot("cumulative_ch4_inc", width = 8, height = 6)
-
+save_plot("cumulative_ch4_by_core", width = 8, height = 6)
 
 # Plot final (end of incubation) totals
 fluxdata_cutoff %>% 
   group_by(Site, TREATMENT_PHASE, SampleID) %>% 
   summarise(inctime_hours = last(inctime_hours),
-            cumCO2_flux_mgC_gSoil = last(cumCO2_flux_mgC_gSoil),
-            cumCH4_flux_mgC_gSoil = last(cumCH4_flux_mgC_gSoil)) ->
+            `cumCO2 flux (mgC/gSoil)` = last(`cumCO2 flux (mgC/gSoil)`),
+            `cumCH4 flux (mgC/gSoil)` = last(`cumCH4 flux (mgC/gSoil)`)) ->
   fluxdata_final
 save_data(fluxdata_final)
 
-p_final_co2 <- ggplot(fluxdata_final, aes(Site, cumCO2_flux_mgC_gSoil, color = Site)) +
+fluxdata_final %>% 
+  group_by(Site, TREATMENT_PHASE) %>%  # should be already grouped...
+  summarise(`cumCO2 flux (mgC/gSoil) s.d.` = sd(`cumCO2 flux (mgC/gSoil)`),
+            `cumCO2 flux (mgC/gSoil)` = mean(`cumCO2 flux (mgC/gSoil)`),
+            `cumCH4 flux (mgC/gSoil) s.d.` = sd(`cumCH4 flux (mgC/gSoil)`),
+            `cumCH4 flux (mgC/gSoil)` = mean(`cumCH4 flux (mgC/gSoil)`)) ->
+  fluxdata_final_by_site
+save_data(fluxdata_final_by_site)
+
+
+p_final_co2 <- ggplot(fluxdata_final, aes(Site, `cumCO2 flux (mgC/gSoil)`, color = Site)) +
   geom_boxplot() + geom_point() +
   facet_wrap(~TREATMENT_PHASE, scales = "free") + theme(strip.text = element_text(size = 7))
 print(p_final_co2)
 save_plot("final_co2_inc", width = 8, height = 6)
-p_final_ch4 <- ggplot(fluxdata_final, aes(Site, cumCH4_flux_mgC_gSoil, color = Site)) +
+p_final_ch4 <- ggplot(fluxdata_final, aes(Site, `cumCH4 flux (mgC/gSoil)`, color = Site)) +
   geom_boxplot() + geom_point() +
   geom_hline(yintercept = 0, linetype = 2) +
   facet_wrap(~TREATMENT_PHASE, scales = "free") + theme(strip.text = element_text(size = 7))
 print(p_final_ch4)
 save_plot("final_ch4_inc", width = 8, height = 6)
-
-
-# DON'T KNOW IF ANYTHING WORKS BEYOND HERE!!!!!
-stop("# DON'T KNOW IF ANYTHING WORKS BEYOND HERE!!!!!")
-
-# Site-level means
-fluxdata %>%
-  filter(!is.na(cumCO2_flux_mgC_gC)) %>% 
-  # We need to first interpolate the fluxes to every hour
-  group_by(TREATMENT_PHASE, Site, SampleID) %>% 
-  filter(n() > 2) %>%   # make sure enough samples to interpolate
-  group_by(TREATMENT_PHASE, Site, SampleID) %>% 
-  do(tibble(label = first(.$label),
-            inctime_hours = seq_len(max(.$inctime_hours)),
-            cumCH4_flux_mgC_gC = approx(.$inctime_hours, .$cumCH4_flux_mgC_gC, xout = inctime_hours, rule = 2)[['y']],
-            cumCO2_flux_mgC_gC = approx(.$inctime_hours, .$cumCO2_flux_mgC_gC, xout = inctime_hours, rule = 2)[['y']])) %>% 
-  # Now can average for each hour
-  group_by(TREATMENT_PHASE, Site, inctime_hours) %>% 
-  summarise(cumCO2_flux_mgC_gC_sd = sd(cumCO2_flux_mgC_gC, na.rm = TRUE),
-            cumCO2_flux_mgC_gC = mean(cumCO2_flux_mgC_gC, na.rm = TRUE),
-            cumCH4_flux_mgC_gC_sd = sd(cumCH4_flux_mgC_gC, na.rm = TRUE),
-            cumCH4_flux_mgC_gC = mean(cumCH4_flux_mgC_gC, na.rm = TRUE)) ->
-  fluxdata_by_site
-
-fd1 <- filter(fluxdata_by_site,
-              TREATMENT_PHASE %in% c("DROUGHT_PRE_INCUBATION", "DROUGHT_INCUBATION", "FIELD_MOIST_INCUBATION", "SATURATION_INCUBATION"))
-fd2 <- filter(fluxdata_by_site,
-              !TREATMENT_PHASE %in% c("DROUGHT_PRE_INCUBATION", "DROUGHT_INCUBATION", "FIELD_MOIST_INCUBATION", "SATURATION_INCUBATION"))
-
-p_site_co2 <- ggplot(fd1, aes(inctime_hours, cumCO2_flux_mgC_gC, color = Site)) +
-  geom_line(size = 1) +
-  geom_ribbon(alpha = I(0.5), color = NA,
-              aes(fill = Site,
-                  ymin = cumCO2_flux_mgC_gC - cumCO2_flux_mgC_gC_sd,
-                  ymax = cumCO2_flux_mgC_gC + cumCO2_flux_mgC_gC_sd)) + 
-  facet_wrap(~TREATMENT_PHASE, scales = "free") + xlab("Incubation (hrs)") +
-  ggtitle("Cumulative CO2 emissions by site")
-print(p_site_co2)
-save_plot("cumulative_co2_site")
-
-p_site_ch4 <- ggplot(fd1, aes(inctime_hours, cumCH4_flux_mgC_gC, color = Site)) +
-  geom_line(size = 1) +
-  geom_ribbon(alpha = I(0.5), color = NA,
-              aes(fill = Site,
-                  ymin = cumCH4_flux_mgC_gC - cumCH4_flux_mgC_gC_sd,
-                  ymax = cumCH4_flux_mgC_gC + cumCH4_flux_mgC_gC_sd)) + 
-  facet_wrap(~TREATMENT_PHASE, scales = "free") + xlab("Incubation (hrs)") +
-  ggtitle("Cumulative CH4 emissions by site")
-print(p_site_ch4)
-save_plot("cumulative_ch4_site")
-
-
-# -----------------------------------------------------------------------------
-# Join with C/N data
-
 
 
 # -----------------------------------------------------------------------------
